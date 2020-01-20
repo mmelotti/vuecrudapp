b66a2b99ebd050dbc57a5a63b7b91bd530144d67
Maison Melotti <maisonmelotti@gmail.com>
2020-01-20 11:31:34 -0300

    routes for crud

--- vueapp/client/routes.js
+++ vueapp/client/routes.js
@@ -3,6 +3,8 @@ import Router from "vue-router";
 import Home from "@pages/Home.vue";
 import Login from "@pages/Login.vue";
 import Register from "@pages/Register.vue";
+import WorkerList from "@pages/WorkerList.vue";
+import WorkerCreate from "@pages/WorkerCreate.vue";
 
 export default new Router({
   mode: "history",
@@ -14,6 +16,14 @@ export default new Router({
     {
       path: "/auth/register",
       component: Register
+    },
+    {
+      path: "/worker/list",
+      component: WorkerList
+    },
+    {
+      path: "/worker/create",
+      component: WorkerCreate
     },
     {
       path: "/",
